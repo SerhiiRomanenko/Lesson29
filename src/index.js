ecb0c4f8538ab447b9de9703fabe3f788b996883
SergeRomanenko13 <serhiiromanenko13@gmail.com>
2021-08-10 16:25:34 +0300

    1st commit

---
+++ src/index.js
@@ -0,0 +1,62 @@
+'use strict';
+
+const menu = {
+    burger: [
+        {
+            name: '🍔',
+            time: 3500,
+        },
+        {
+            name: '🍟',
+            time: 1500,
+        },
+        {
+            name: '🍵',
+            time: 500,
+        },
+    ],
+    pizza: [
+        {
+            name: '🍕',
+            time: 2000,
+        },
+        {
+            name: '🍵',
+            time: 500,
+        },
+    ],
+    buritto: [
+        {
+            name: '🍞',
+            time: 4200,
+        },
+        {
+            name: '🍟',
+            time: 1500,
+        },
+        {
+            name: '🍵',
+            time: 500,
+        },
+    ],
+};
+
+const result = [];
+function order(food, callback) {
+    console.log(`Ваш заказ получен. Начинаем готовить…`);
+    for (const item in food) {
+        setTimeout(() => {
+            console.log(`${food[item].name} приготовлен(о)`);
+            result.unshift({ value: food[item].name });
+            if (result.length === food.length) {
+                callback(result);
+            }
+        }, food[item].time);
+    }
+}
+
+const cb = data => {
+    console.log('Готово! Заберите ваш заказ', data);
+};
+
+order(menu.pizza, cb);
